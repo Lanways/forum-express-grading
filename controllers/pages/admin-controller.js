71dcfbd3c9a422e9305f5ba1b71b021307ec6837
Lanways <mcbluedd@gmail.com>
2023-05-30 15:06:44 +0800

    feat: add admin-services

--- controllers/pages/admin-controller.js
+++ controllers/pages/admin-controller.js
@@ -1,18 +1,10 @@
 const { Restaurant, User, Category } = require('../../models')
-// const { User } = require('../models/user')
-const { localFileHandler } = require('../../helpers/file-helpers')
 const { imgurFileHandler } = require('../../helpers/file-helpers')
-const category = require('../../models/category')
+const adminServices = require('../../services/admin-services')
 
 const adminController = {
   getRestaurants: (req, res, next) => {
-    Restaurant.findAll({
-      raw: true,
-      nest: true,
-      include: [Category]
-    })
-      .then(restaurants => res.render('admin/restaurants', { restaurants }))
-      .catch(err => next(err))
+    adminServices.getRestaurants(req, (err, data) => err ? next(err) : res.render('admin/restaurants', data))
   },
   createRestaurant: (req, res) => {
     return Category.findAll({ raw: true })
