b50f62c738ae92fca68ed2c51c145eabbc99b7d0
Lanways <mcbluedd@gmail.com>
2023-05-30 12:24:14 +0800

    feat: add getRestaurants api

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const express = require('express')
-const { pages } = require('./routes')
+const { pages, apis } = require('./routes')
 const handlebars = require('express-handlebars')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const methodOverride = require('method-override')
@@ -31,6 +31,7 @@ app.use((req, res, next) => {
   next()
 })
 
+app.use('/api', apis)
 app.use(pages)
 
 app.listen(port, () => {
